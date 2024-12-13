5d71392b5fee044bae343126c896dee633fffdd4
Unity-LRTools <pro.lucasrossignol@gmail.com>
2024-12-13 18:42:00 +0100

    Remove specials tags and move modifier to rarity file

--- Runtime/Data/RarityData.cs
+++ Runtime/Data/RarityData.cs
@@ -10,4 +10,17 @@ namespace LRT.Smith.Equipments
 		string name;
 		List<Modifier> modifier;
 	}
+
+	[Serializable]
+	public class Modifier
+	{
+		Type modifierType;
+		float modifier;
+
+		public enum Type
+		{
+			Offset,
+			Percentage,
+		}
+	}
 }
